84c6f20a6fbafa08a1aae4bf14b8bc17e6d51ba8
ChSPN <charlene.scomparin@gmail.com>
2024-05-17 11:24:47 +0200

    add the docstrings

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+
+Its purpose is to define a square with a certain size.
+"""
+
+
 class Square:
+    """
+    This is a Square class.
+
+    It defines a square of a certain size.
+    """
+
     def __init__(self, size=0):
+        """
+        This is the constructor method for Square class.
+
+        It initializes a Square with a given size.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
