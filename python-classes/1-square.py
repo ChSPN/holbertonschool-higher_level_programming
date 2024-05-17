4f4de77263eb1bfaa79e6fda2f9b557ae3ccf553
ChSPN <charlene.scomparin@gmail.com>
2024-05-17 08:43:02 +0200

    fix the code with requirements

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size):
-        self.__size = size
+"""
+This module defines a Square class.
 
+Its purpose is to define a square with a certain size.
+"""
+
+
+class Square:
+    """
+    This is a Square class.
+
+    It defines a square of a certain size.
+    """
+
+    def __init__(self, size):
+        """
+        This is the constructor method for Square class.
+
+        It initializes a Square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
