0b0468bdd63fea6ed3846f0b9ab5b1c47dd2b06b
ChSPN <charlene.scomparin@gmail.com>
2024-05-21 11:17:21 +0200

    fix errors

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -70,24 +70,22 @@ class Rectangle:
             return 0
         return 2 * (self.__width + self.__height)
 
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle instance using the
+        character(s) stored in print_symbol.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join([str(self.print_symbol) * self.__width
+                          for _ in range(self.__height)])
 
-def __str__(self):
-    """
-    Return a string representation of the Rectangle instance using the
-    character(s) stored in print_symbol.
-    """
-    if self.__width == 0 or self.__height == 0:
-        return ""
-    return "\n".join([str(self.print_symbol) * self.__width
-                      for _ in range(self.__height)])
-
-
-def __repr__(self):
-    """
-    Return a string representation of the Rectangle instance that can be
-    used with eval to create a new instance.
-    """
-    return "Rectangle({}, {})".format(self.__width, self.__height)
+    def __repr__(self):
+        """
+        Return a string representation of the Rectangle instance that can be
+        used with eval to create a new instance.
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         """
