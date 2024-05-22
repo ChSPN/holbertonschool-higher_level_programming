d5bbca9780b7761e9e04e26cffa965e8f4bdee1a
ChSPN <charlene.scomparin@gmail.com>
2024-05-22 10:51:02 +0200

    Write a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Ce module définit la classe `Square` qui hérite de `Rectangle`.
+Il ne fait pas usage des mots 'import' ou 'from' dans les commentaires.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Une classe `Square` qui hérite de `Rectangle`.
+    Elle est initialisée avec une taille qui doit être un entier positif.
+    """
+
+    def __init__(self, size):
+        """
+        Initialise une nouvelle instance de `Square`.
+
+        Args:
+            size (int): La taille du carré. Doit être un entier positif.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Retourne l'aire du carré.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Retourne une description du carré sous la forme [Square] <size>/<size>.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
