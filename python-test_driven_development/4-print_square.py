e6830ab906d616315556daf1c4164169895063fb
ChSPN <charlene.scomparin@gmail.com>
2024-05-23 17:55:55 +0200

    Write a function that prints a square with the character #.

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""
-Ce module contient une fonction qui imprime un carré de la taille spécifiée.
-
-La fonction print_square prend un argument, la taille du carré.
-Si la taille n'est pas un entier, elle lève une exception TypeError.
-Si la taille est inférieure à 0, elle lève une exception ValueError.
-"""
+"""Module documentation"""
 
 
 def print_square(size):
-    """
-    Imprime un carré de la taille spécifiée.
-
-    Args:
-        size (int): La taille du carré à imprimer.
-
-    Raises:
-        TypeError: Si la taille n'est pas un entier.
-        ValueError: Si la taille est inférieure à 0.
-    """
-    if not isinstance(size, int):
+    """Function documentation"""
+    e = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if size is None:
+        raise TypeError(e)
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    for _ in range(size):
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
         print("#" * size)
