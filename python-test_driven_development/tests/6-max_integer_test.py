44508c75f5bf4b20709dbc8576825476e2410554
ChSPN <charlene.scomparin@gmail.com>
2024-05-24 10:24:54 +0200

    try to fix a fail

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])"""
+"""Unittest for max_integer([..])
+"""
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+
+    def test_regular_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
     def test_empty_list(self):
-        """Test with an empty list"""
         self.assertIsNone(max_integer([]))
 
     def test_single_element_list(self):
-        """Test with a list of one element"""
-        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([5]), 5)
 
-    def test_same_element_list(self):
-        """Test with a list of identical elements"""
-        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -5, -3, -2]), -1)
 
-    def test_sorted_list(self):
-        """Test with a list sorted in ascending order"""
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_duplicate_max(self):
+        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
 
-    def test_reverse_sorted_list(self):
-        """Test with a list sorted in descending order"""
-        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([1, -2, 10, 0]), 10)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
