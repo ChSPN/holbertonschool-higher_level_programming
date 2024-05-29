b1f812467f8d10353be040a90cf235b8a1e1ed8b
ChSPN <charlene.scomparin@gmail.com>
2024-05-29 10:41:38 +0200

    Develop a simple API using Python with the `http.server` module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import json
+
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+# Create an object of the above class
+handler_object = MyHttpRequestHandler
+
+PORT = 8000
+my_server = socketserver.TCPServer(("localhost", PORT), handler_object)
+
+# Star the server
+my_server.serve_forever()
