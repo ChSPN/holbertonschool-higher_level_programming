2bb889793870a2e005e6568d444abcef3613dc86
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 09:36:43 +0200

    fix the code

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,17 @@
 -- Genres that don’t have any shows linked are not displayed.
 -- The results are sorted in descending order by the number of shows linked.
 -- Only one SELECT statement is used.
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM genres JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id GROUP BY genres.name HAVING COUNT(tv_show_genres.show_id) > 0 ORDER BY COUNT(tv_show_genres.show_id) DESC;
+
+SELECT
+    genres.name AS genre,
+    COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM
+    genres
+JOIN
+    tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY
+    genres.name
+HAVING
+    COUNT(tv_show_genres.tv_show_id) > 0
+ORDER BY
+    number_of_shows DESC;
