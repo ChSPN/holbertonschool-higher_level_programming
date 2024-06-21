8bea8a5fd9b268cf47ae632f923b9acb97416f0e
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 10:29:26 +0200

    fix

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,15 +1,15 @@
--- Ce script SQL liste tous les spectacles et tous les genres liés à ce spectacle, à partir de la base de données 'hbtn_0d_tvshows'.
--- Si un spectacle n'a pas de genre, 'NULL' est affiché dans la colonne du genre.
--- Chaque enregistrement affiche : 'tv_shows.title' - 'genres.name'.
--- Les résultats sont triés par ordre croissant par le titre du spectacle et le nom du genre.
--- Seule une instruction SELECT est utilisée.
-SELECT
-    tv_shows.title,
-    tv_genres.name
-FROM
+-- Lists all shows and all genres linked to that show from the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL in the genre column.
+-- Each record displays: tv_shows.title - tv_genres.name.
+-- Results are sorted in ascending order by the show title and genre name.
+SELECT 
+    tv_shows.title, -- Select the show title
+    tv_genres.name  -- Select the genre name
+FROM 
     tv_shows
+    -- Perform a LEFT JOIN to include shows without genres
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    LEFT JOIN tv_genres ON tv_show_genres.genre_id = genres.id
-ORDER BY
-    tv_shows.title ASC,
-    tv_genres.name ASC;
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY 
+    tv_shows.title ASC, -- Sort the results in ascending order by show title
+    tv_genres.name ASC; -- Sort the results in ascending order by genre name
