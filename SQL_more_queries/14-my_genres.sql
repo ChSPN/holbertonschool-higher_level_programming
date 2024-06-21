99cbb4da018d3dd1aa4a1716991f575665b5b93f
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 10:06:56 +0200

    fix the code

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,9 @@
 -- Only one SELECT statement is used.
 SELECT tv_genres.name -- Select the genre name
 FROM tv_shows
-    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id 
-    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
-WHERE tv_shows.title = 'Dexter' 
-ORDER BY tv_genres.name ASC;
+    -- Join the tv_shows table with the tv_show_genres table on the show_id
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+    -- Join the tv_show_genres table with the tv_genres table on the genre_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'-- Filter the results to include only the show titled "Dexter"
+ORDER BY tv_genres.name ASC;-- Sort the results in ascending order by the genre name
