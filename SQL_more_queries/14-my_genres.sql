ac0461d702d4cf9663b5a95c7981c2c0e8eec75e
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 10:02:18 +0200

    fix the code

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,10 @@
 -- This SQL statement uses the 'hbtn_0d_tvshows' database to list all genres of the show 'Dexter'.
 -- The 'tv_shows' table contains only one record where 'title' = 'Dexter' (but the 'id' can be different).
--- Each record displays: 'tv_genres.name'.
 -- The results are sorted in ascending order by the genre name.
 -- Only one SELECT statement is used.
-SELECT genres.name 
-FROM genres 
-JOIN tv_show_genres 
-ON genres.id = tv_show_genres.genre_id 
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id 
+SELECT tv_genres.name 
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id 
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
 WHERE tv_shows.title = 'Dexter' 
-ORDER BY genres.name ASC;
+ORDER BY tv_genres.name ASC;
