a834cbeedacf9319e2a0dd193a7463ac2cdf73c6
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 10:14:40 +0200

    fix the code

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -10,6 +10,6 @@ FROM
     JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     JOIN genres ON tv_show_genres.genre_id = genres.id
 WHERE
-    genres.name = 'Comedy'
+    tv_genres.name = 'Comedy'
 ORDER BY
     tv_shows.title ASC;
