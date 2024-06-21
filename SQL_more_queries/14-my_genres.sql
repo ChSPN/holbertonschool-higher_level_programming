93f1ecbc09b403d2b55b1e4fa904812aaf33abd8
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 10:04:20 +0200

    fix the code

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,9 +2,9 @@
 -- The 'tv_shows' table contains only one record where 'title' = 'Dexter' (but the 'id' can be different).
 -- The results are sorted in ascending order by the genre name.
 -- Only one SELECT statement is used.
-SELECT tv_genres.name 
+SELECT tv_genres.name -- Select the genre name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id 
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id 
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
 WHERE tv_shows.title = 'Dexter' 
 ORDER BY tv_genres.name ASC;
