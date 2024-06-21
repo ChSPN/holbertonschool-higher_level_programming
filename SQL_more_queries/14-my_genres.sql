4437b1743d88cb7923915bfb76d2e6981e500344
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 09:51:55 +0200

    try to fix

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,10 @@
 -- Each record displays: 'tv_genres.name'.
 -- The results are sorted in ascending order by the genre name.
 -- Only one SELECT statement is used.
-SELECT genres.name FROM genres JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter' ORDER BY genres.name ASC;
+SELECT genres.name 
+FROM genres 
+JOIN tv_show_genres 
+ON genres.id = tv_show_genres.genre_id 
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id 
+WHERE tv_shows.title = 'Dexter' 
+ORDER BY genres.name ASC;
