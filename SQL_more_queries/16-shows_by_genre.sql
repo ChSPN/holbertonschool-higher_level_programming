1363d9514ed635e8079737be24c5cab980e29945
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 10:25:57 +0200

    fix the code

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,13 @@
 -- Chaque enregistrement affiche : 'tv_shows.title' - 'genres.name'.
 -- Les résultats sont triés par ordre croissant par le titre du spectacle et le nom du genre.
 -- Seule une instruction SELECT est utilisée.
-SELECT tv_shows.title, genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN genres ON tv_show_genres.genre_id = genres.id ORDER BY tv_shows.title ASC, genres.name ASC;
+SELECT
+    tv_shows.title,
+    tv_genres.name
+FROM 
+    tv_shows 
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = genres.id
+ORDER BY
+    tv_shows.title ASC
+    tv_genres.name ASC;
