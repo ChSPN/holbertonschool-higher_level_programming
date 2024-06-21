6073fa182da41093927c1e431b21ffb8eda77bb5
ChSPN <charlene.scomparin@gmail.com>
2024-06-21 10:10:06 +0200

    fix the code

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -3,4 +3,13 @@
 -- Each record displays: 'tv_shows.title'.
 -- The results are sorted in ascending order by the show title.
 -- Only one SELECT statement is used.
-SELECT tv_shows.title FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN genres ON tv_show_genres.genre_id = genres.id WHERE genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
+SELECT
+    tv_shows.title
+FROM
+    tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN genres ON tv_show_genres.genre_id = genres.id
+WHERE
+    genres.name = 'Comedy'
+ORDER BY
+    tv_shows.title ASC;
